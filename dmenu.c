e415b74fe41e45060aba8ab08f3a3d7a442d446a
Sebastian LaVine <mail@smlavine.com>
2021-07-03 17:35:50 -0400

    Apply dmenu-linesbelowprompt-20210703-1a13d04.diff
    https://tools.suckless.org/dmenu/patches/lines-below-prompt/dmenu-linesbelowprompt-20210703-1a13d04.diff

--- dmenu.c
+++ dmenu.c
@@ -227,7 +227,7 @@ drawmenu(void)
 		for (item = curr; item != next; item = item->right, i++)
 			drawitem(
 				item,
-				x + ((i / lines) *  ((mw - x) / columns)),
+				x - promptw + ((i / lines) *  ((mw - x) / columns)),
 				y + (((i % lines) + 1) * bh),
 				(mw - x) / columns
 			);
